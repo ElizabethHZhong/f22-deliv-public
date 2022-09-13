a1673888d5f0646575803cdfc25c8c76b21eb174
ElizabethHZhong <89757780+ElizabethHZhong@users.noreply.github.com>
2022-09-12 20:35:14 -0400

    update

--- src/components/EntryModal.js
+++ src/components/EntryModal.js
@@ -13,7 +13,8 @@ import TextField from '@mui/material/TextField';
 import * as React from 'react';
 import { useState } from 'react';
 import { categories } from '../utils/categories';
-import { addEntry } from '../utils/mutations';
+import { addEntry, updateEntry, deleteEntry } from '../utils/mutations';
+import QRCode from 'qrcode';
 
 // Modal component for individual entries.
 
@@ -28,13 +29,29 @@ export default function EntryModal({ entry, type, user }) {
 
    // State variables for modal status
 
-   // TODO: For editing, you may have to add and manage another state variable to check if the entry is being edited.
+   // TODO: For editing, you may have to add and manage another state variable 
+   // to check if the entry is being edited.
 
    const [open, setOpen] = useState(false);
    const [name, setName] = useState(entry.name);
    const [link, setLink] = useState(entry.link);
    const [description, setDescription] = useState(entry.description);
    const [category, setCategory] = React.useState(entry.category);
+   const [edit, setEdit] = useState(false);
+   const [text, setText] = useState('');
+   const [imageUrl, setImageUrl] = useState(''); // QR code state variable
+   const [showQR, setShowQR] = useState(false); // QR code state variable
+
+   
+   const generateQrCode = async () => {
+      try {
+         const response = await QRCode.toDataURL(text);
+         setImageUrl(response);
+      }catch (error) {
+         console.log(error);
+      }
+   }
+   
 
    // Modal visibility handlers
 
@@ -44,10 +61,12 @@ export default function EntryModal({ entry, type, user }) {
       setLink(entry.link);
       setDescription(entry.description);
       setCategory(entry.category);
-   };
+;   };
 
    const handleClose = () => {
       setOpen(false);
+      setEdit(false);
+      setShowQR(false);
    };
 
    // Mutation handlers
@@ -67,8 +86,50 @@ export default function EntryModal({ entry, type, user }) {
    };
 
    // TODO: Add Edit Mutation Handler
+   const handleEdit = () => {
+      setEdit(true);
+   };
+
+   const handleConfirm = () => {
+      const newEntry = {
+         name: name,
+         link: link,
+         description: description,
+         user: user?.displayName ? user?.displayName : "GenericUser",
+         category: category,
+         userid: user?.uid,
+         id: entry.id,
+      };
+      updateEntry(newEntry).catch(console.error);
+      handleClose();
+   };
 
    // TODO: Add Delete Mutation Handler
+   const handleDelete = () => {
+      deleteEntry(entry).catch(console.error);
+      handleClose();
+   };
+
+   const handleAlert = () => {
+      if (window.confirm("Confirm to delete.")) {
+         handleDelete();
+      }
+   }
+
+   // TODO: QR Code Handler
+   const handleQR = () => {
+      setText(entry.link)
+      console.log(entry.link)
+      generateQrCode();
+      setShowQR(true);
+   }
+
+   const twoCalls = (e) => {
+      setLink(e); 
+      setText(e);
+      generateQrCode();
+   }
+
 
    // Button handlers for modal opening and inside-modal actions.
    // These buttons are displayed conditionally based on if adding or editing/opening.
@@ -87,6 +148,17 @@ export default function EntryModal({ entry, type, user }) {
       type === "edit" ?
          <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleAlert}>Delete</Button>
+            {edit? (
+               <Button variant="contained" size="small" onClick={handleConfirm}>
+                  Confirm
+               </Button>
+            ) : (
+               <Button variant="contained" size="small" onClick={handleEdit}>
+                  Edit
+               </Button>
+            )}
+            
          </DialogActions>
          : type === "add" ?
             <DialogActions>
@@ -94,6 +166,18 @@ export default function EntryModal({ entry, type, user }) {
                <Button variant="contained" onClick={handleAdd}>Add Entry</Button>
             </DialogActions>
             : null;
+   
+   let qrimage;
+   if (!imageUrl) {
+      qrimage = <p>Type in link to generate a QR code.</p>
+   } else {
+      qrimage = <img 
+                  style={{ width: 150, height: 150 }}
+                  src={imageUrl} 
+                  alt="img"
+                />
+   }
+
 
    return (
       <div>
@@ -109,6 +193,9 @@ export default function EntryModal({ entry, type, user }) {
                   fullWidth
                   variant="standard"
                   value={name}
+                  InputProps={{
+                     readOnly: type === "edit" ? !edit : edit
+                  }}
                   onChange={(event) => setName(event.target.value)}
                />
                <TextField
@@ -119,7 +206,10 @@ export default function EntryModal({ entry, type, user }) {
                   fullWidth
                   variant="standard"
                   value={link}
-                  onChange={(event) => setLink(event.target.value)}
+                  InputProps={{
+                     readOnly: type === "edit" ? !edit : edit
+                  }}
+                  onChange={(event) => twoCalls(event.target.value)}
                />
                <TextField
                   margin="normal"
@@ -130,6 +220,9 @@ export default function EntryModal({ entry, type, user }) {
                   multiline
                   maxRows={8}
                   value={description}
+                  InputProps={{
+                     readOnly: type === "edit" ? !edit : edit
+                  }}
                   onChange={(event) => setDescription(event.target.value)}
                />
 
@@ -140,12 +233,49 @@ export default function EntryModal({ entry, type, user }) {
                      id="demo-simple-select"
                      value={category}
                      label="Category"
+                     inputProps={{
+                        readOnly: type === "edit" ? !edit : edit
+                     }}
                      onChange={(event) => setCategory(event.target.value)}
                   >
                      {categories.map((category) => (<MenuItem value={category.id}>{category.name}</MenuItem>))}
                   </Select>
                </FormControl>
             </DialogContent>
+            {showQR ? (
+               <div
+                  style={{
+                     display: 'grid',
+                     alignItems: 'center',
+                     justifyContent: 'center',
+                  }}
+               >
+                     {qrimage}
+               </div> 
+            ) : (
+               <Button variant="contained" size="large" onClick={handleQR}>
+                  Generate QR Code
+               </Button>
+            )}
+            {showQR ? (
+               <div
+               style={{ 
+                  display: 'grid',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+               }} 
+               >
+               <a href={imageUrl} download>
+                  <Button 
+                     variant="contained" 
+                     size="medium" 
+                  >
+                     Download
+                  </Button>
+               </a>
+            </div>
+            ) : null}
+                  
             {actionButtons}
          </Dialog>
       </div>
